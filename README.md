ceaf14b9f9d653f5686e936769d753004276a4c0
firesquid6 <jdeiss06@gmail.com>
2024-08-28 23:31:55 -0500

    add some more comments

---
+++ README.md
@@ -0,0 +1,11 @@
+# CSI-1430
+
+This is all of the code I wrote for assignments in CSI-1430. 
+
+
+# Tooling
+
+I imagine most of the people here are peers who want to know what my tooling is. I don't like eclipse and use neovim with nixos. My workflow is:
+
+1. create a new project using `new-program.sh` in the scripts directory.
+2. 
--- template/main.cpp
+++ template/main.cpp
@@ -1,15 +1,19 @@
 /*
-* Author: Jonathan Deiss
-* Assignment Title: 
-* Assignment Description: 
-* Due Date: 
-* Date Created: 
-* Date Last Modified: 
+ * Author: Jonathan Deiss
+ * Assignment Title:
+ * Assignment Description:
+ * Due Date:
+ * Date Created:
+ * Date Last Modified:
  */
 #include <iostream>
 
 using namespace std;
-
 int main() {
+  // Data Abstraction
+  // Input
+  // Process
+  // Output
+
   return 0;
 }

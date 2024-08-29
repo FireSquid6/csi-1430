0829bc3f946f0d71962fe7d40ec369286212ef15
Jonathan Deiss <jdeiss@gmail.com>
2024-08-29 12:42:36 -0500

    add program-1

--- programming-assignments/program-1/main.cpp
+++ programming-assignments/program-1/main.cpp
@@ -1,19 +1,27 @@
 /*
- * Author: Jonathan Deiss
- * Assignment Title:
- * Assignment Description:
- * Due Date:
- * Date Created:
- * Date Last Modified:
+* Author: Jonathan Deiss
+* Assignment Title: Hello World
+* Assignment Description: Prints hello world.
+* Due Date: 8/28/2024
+* Date Created: 8/28/2024
+* Date Last Modified: 8/28/2024
  */
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
+
 int main() {
   // Data Abstraction
   // Input
   // Process
   // Output
-  cout << "Hello, World!" << endl;
+  cout << "Hello World." << endl;
+  cout << "Salut le Monde!" << endl;
+  cout << "Hallo Welt!!" << endl;
+  cout << "Geia soy kosme!!!" << endl;
+  cout << "Shalom Olam" << endl;
+
   return 0;
+
 }

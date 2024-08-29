6b46fa1cb100066c4d60f48263ebd3eabcad31e9
firesquid6 <jdeiss06@gmail.com>
2024-08-28 23:26:44 -0500

    add new program system

---
+++ programming-assignments/program-1/main.cpp
@@ -0,0 +1,15 @@
+/*
+* Author: Jonathan Deiss
+* Assignment Title: 
+* Assignment Description: 
+* Due Date: 
+* Date Created: 
+* Date Last Modified: 
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  return 0;
+}
--- scripts/new-program.sh
+++ scripts/new-program.sh
@@ -2,7 +2,6 @@
 
 cd "$(dirname "$0")/.."
 
-# ensure that $1 exists
 if [ -z "$1" ]; then
   echo "Usage: $0 <assignment-name> [--in-class] [--scratch]"
   exit 1
@@ -18,4 +17,12 @@ if [[ $* == *--scratch* ]]; then
   DIRECTORY="./scratch/$1"
 fi
 
-echo "$DIRECTORY"
+if [ -d "$DIRECTORY" ]; then
+  echo "Directory $DIRECTORY already exists"
+  exit 1
+fi
+
+mkdir -p "$DIRECTORY"
+
+# copy all files from the template directory
+cp -r ./template/. "$DIRECTORY"

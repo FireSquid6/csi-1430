9bb98315ca60f2fd0c35a9a0ff4b07f409c3a271
Jonathan Deiss <jdeiss@gmail.com>
2024-08-30 09:22:01 -0500

    create days-lived

---
+++ in-class-assignments/days-lived/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title:
+ * Assignment Description:
+ * Due Date:
+ * Date Created:
+ * Date Last Modified:
+ */
+#include <iostream>
+
+using namespace std;
+int main() {
+  // Data Abstraction:
+  int yearsLived;
+    
+  // Input:
+  cin >> yearsLived;
+    
+  // Process:
+    
+  // Output:
+  cout << "Age: " << yearsLived << endl;
+  cout << "Days Lived: " << yearsLived * 365 << endl;
+    
+  // Assumptions:
+
+  return 0;
+}

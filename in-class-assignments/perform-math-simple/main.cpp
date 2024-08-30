48217cf9cd5740d54eb93d8c7e4e70550a41d08c
Jonathan Deiss <jdeiss@gmail.com>
2024-08-30 09:19:43 -0500

    add the program she wanted us to make

---
+++ in-class-assignments/perform-math-simple/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title:
+ * Assignment Description:
+ * Due Date:
+ * Date Created:
+ * Date Last Modified:
+ */
+#include <iostream>
+
+using namespace std;
+int main() {
+  // Data Abstraction:
+  int num1, num2;
+  int answer = 0;
+    
+  // Input:
+  cin >> num1;
+  cin >> num2;
+    
+  // Output:
+  cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+  cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+  cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+  cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+    
+  // Assumptions:
+
+  return 0;
+}

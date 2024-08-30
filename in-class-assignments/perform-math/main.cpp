b0775baaee6a0a1f9860f34a4b879afefef41cbb
Jonathan Deiss <jdeiss@gmail.com>
2024-08-30 09:15:47 -0500

    add a version before she tells us what to do in class

--- in-class-assignments/perform-math/main.cpp
+++ in-class-assignments/perform-math/main.cpp
@@ -1,24 +1,54 @@
 /*
  * Author: Jonathan Deiss
- * Assignment Title:
- * Assignment Description:
- * Due Date:
- * Date Created:
- * Date Last Modified:
+ * Assignment Title: Example One
+ * Assignment Description: Mathematical operations
+ * Due Date: 08/30/2024
+ * Date Created: 08/30/2024
+ * Date Last Modified: 08/30/2024
  */
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 int main() {
   // Data Abstraction:
-    
+  double num1, num2;
+  double solution = 0;
+  char op;
+  bool success = true;
+
   // Input:
-    
+  cout << "Input two numbers and then the operator (+, -, *, /):" << endl;
+  cin >> num1;
+  cin >> num2;
+  cin >> op;
+
   // Process:
-    
+  switch (op) {
+    case '+':
+      solution = num1 + num2;
+      break;
+    case '-':
+      solution = num1 - num2;
+      break;
+    case '/':
+      solution = num1 / num2;
+      break;
+    case '*':
+      solution = num1 * num2;
+      break;
+    default:
+      success = false;
+      break;
+  }
+
   // Output:
-    
-  // Assumptions:
+  cout << fixed << setprecision(3);
+  if (success) {
+    cout << num1 << " " << op << " " << num2 << " = " << solution << endl;
+  } else {
+    cout << "Invalid input" << endl;
+  }
 
   return 0;
 }

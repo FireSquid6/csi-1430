0e8d645b519291c64d15bcfe48947fb60b830695
Jonathan Deiss <jdeiss@gmail.com>
2024-08-30 09:30:31 -0500

    add program to calclulate the area of a circle

---
+++ in-class-assignments/circle-area/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title:
+ * Assignment Description:
+ * Due Date:
+ * Date Created:
+ * Date Last Modified:
+ */
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main() {
+  // Data Abstraction:
+  double radius;
+  double area;
+  const double PI = 3.14159;
+    
+  // Input:
+  cin >> radius;
+    
+  // Process:
+  area = PI * radius * radius;
+    
+  // Output:
+  cout << fixed << setprecision(3);
+  cout << "Circle with radius " << radius << " has an area of " << area << endl;
+    
+  // Assumptions:
+
+  return 0;
+}

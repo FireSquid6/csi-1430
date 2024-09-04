f2b448e766ee6efd216906b2db1c23d4da020068
firesquid6 <jdeiss06@gmail.com>
2024-09-04 09:18:22 -0500

    add a dumb test

---
+++ in-class-assignments/stupid=test/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title:
+ * Assignment Description:
+ * Due Date:
+ * Date Created:
+ * Date Last Modified:
+ */
+#include <iostream>
+
+using namespace std;
+int main() {
+  // Data Abstraction:
+  unsigned long x = 100;
+    
+  // Input:
+  x = -20;
+    
+  // Process:
+    
+  // Output:
+  cout << x << endl;
+    
+  // Assumptions:
+
+  return 0;
+}

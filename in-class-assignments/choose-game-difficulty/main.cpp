39ede031137f47e65fc6e344e97f1c5d060fc4e9
firesquid6 <jdeiss06@gmail.com>
2024-09-16 09:34:20 -0500

    add difficulty selection minigame

--- in-class-assignments/choose-game-difficulty/main.cpp
+++ in-class-assignments/choose-game-difficulty/main.cpp
@@ -13,6 +13,41 @@ int main() {
   // Data Abstraction:
     
   // Input:
+  bool looping = true;
+
+  while (looping) {
+    cout << "Please select a difficulty level:" << endl;
+    cout << "Easy" << endl;
+    cout << "Medium" << endl;
+    cout << "Hard" << endl;
+    cout << endl;
+
+    string difficulty;
+    cin >> difficulty;
+
+    cout << endl;
+    if (difficulty == "Easy") {
+      cout << "You have selected Easy." << endl;
+    } else if (difficulty == "Medium") {
+      cout << "You have selected Medium." << endl;
+    } else if (difficulty == "Hard") {
+      cout << "You have selected Hard." << endl;
+    } else {
+      cout << "Invalid difficulty level." << endl;
+    }
+
+    cout << "Would you like to select another difficulty level? (y/n)" << endl;
+    char response;
+    cin >> response;
+
+    if (response == 'n') {
+      looping = false;
+    } else if (response != 'y') {
+      cout << "Invalid response." << endl;
+    }
+
+    cout << endl;
+  }
     
   // Process:
     

3961bfe3acc73fbd61bed9b245f74f6549a2d1f6
firesquid6 <jdeiss06@gmail.com>
2024-09-16 11:00:51 -0500

    add valid

--- programming-assignments/program-7/main.cpp
+++ programming-assignments/program-7/main.cpp
@@ -6,10 +6,10 @@
  * Date Created: 09/16/2024
  * Date Last Modified: 09/16/2024
  */
-#include <iostream>
-#include <iomanip>
-#include <vector>
 #include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 int main() {
@@ -18,19 +18,24 @@ int main() {
 
   double average;
   char letterGrade;
-  double minimum; 
-  double maximium; 
+  double minimum;
+  double maximium;
   double median;
-    
+  bool valid = true;
+
   // Input:
-  cout << "Please Enter Five Test Scores." << endl; 
+  cout << "Please Enter Five Test Scores." << endl;
 
   for (int i = 0; i < 5; i++) {
     double temp;
     cin >> temp;
+
+    if (temp < 0 || temp > 100) {
+      valid = false;
+    }
     scores.push_back(temp);
   }
-    
+
   // echo input
   cout << fixed << setprecision(2);
   for (unsigned int i = 0; i < scores.size(); i++) {
@@ -39,8 +44,8 @@ int main() {
   cout << endl << endl;
 
   // Process:
-  // sorting is technically not necessary, since median, mean, max, and min 
-  // can all be found in O(n) time. finding the median is kinda hard though, 
+  // sorting is technically not necessary, since median, mean, max, and min
+  // can all be found in O(n) time. finding the median is kinda hard though,
   // so we'll just sort the vector
   //
   // https://en.wikipedia.org/wiki/Median_of_medians
@@ -48,7 +53,7 @@ int main() {
   sort(scores.begin(), scores.end(), greater<double>());
 
   maximium = scores.at(0);
-  minimum = scores.at(scores.size() -1);
+  minimum = scores.at(scores.size() - 1);
   median = scores.at(scores.size() / 2);
   for (int i = 0; i < 5; i++) {
     average += scores.at(i);
@@ -56,34 +61,36 @@ int main() {
   average /= 5;
 
   switch (int(average) / 10) {
-    case 10:
-    case 9:
-      letterGrade = 'A';
-      break;
-    case 8:
-      letterGrade = 'B';
-      break;
-    case 7:
-      letterGrade = 'C';
-      break;
-    case 6:
-      letterGrade = 'D';
-      break;
-    default:
-      letterGrade = 'F';
-      break;
+  case 10:
+  case 9:
+    letterGrade = 'A';
+    break;
+  case 8:
+    letterGrade = 'B';
+    break;
+  case 7:
+    letterGrade = 'C';
+    break;
+  case 6:
+    letterGrade = 'D';
+    break;
+  default:
+    letterGrade = 'F';
+    break;
   }
-    
-  // Output:
-  cout << "Average = " << average << endl;
-  cout << "Grade   = " << letterGrade << endl;
-  cout << "Min     = " << minimum << endl;
-  cout << "Max     = " << maximium << endl;
-  cout << "Median  = " << median << endl;
 
-    
+  // Output:
+  if (valid) {
+    cout << "Average = " << average << endl;
+    cout << "Grade   = " << letterGrade << endl;
+    cout << "Min     = " << minimum << endl;
+    cout << "Max     = " << maximium << endl;
+    cout << "Median  = " << median << endl;
+  } else {
+    cout << "ERROR: BAD DATA" << endl;
+  }
+
   // Assumptions:
-  // - nothing in scores is negative or greater than 100.0
   // - everything in scores is a valid double that can be parsed
 
   return 0;

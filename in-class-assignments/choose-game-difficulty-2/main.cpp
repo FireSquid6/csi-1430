aea6be64d5dace33a548069ffe35190c6722c0e7
firesquid6 <jdeiss06@gmail.com>
2024-09-16 09:53:55 -0500

    add her version of difficulty selector

--- in-class-assignments/choose-game-difficulty-2/main.cpp
+++ in-class-assignments/choose-game-difficulty-2/main.cpp
@@ -11,13 +11,42 @@
 using namespace std;
 int main() {
   // Data Abstraction:
-    
+  char choice = 'y';
+  int difficultyLevel;
+
   // Input:
-    
+
   // Process:
-    
+
   // Output:
-    
+  while (choice == 'y') {
+    cout << "1. Easy" << endl;
+    cout << "2. Medium" << endl;
+    cout << "3. Hard" << endl;
+    cout << "Choose the Difficulty Level:" << endl;
+
+    cin >> difficultyLevel;
+
+    switch (difficultyLevel) {
+      case 1:
+        cout << " EASY " << endl;
+        break;
+      case 2:
+        cout << " MEDIUM " << endl;
+        break;
+      case 3:
+        cout << " HARD " << endl;
+        break;
+      default:
+        cout << "Please choose a valid difficulty level:" << endl;
+        cin >> difficultyLevel;
+        break;
+    }
+
+    cout << "Would you like to th choose again? (y/n):" << endl;
+    cin >> choice;
+
+  }
   // Assumptions:
 
   return 0;

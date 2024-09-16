3ebebff9476fbf52f6ecc2c62b0360aa26c52147
firesquid6 <jdeiss06@gmail.com>
2024-09-16 09:47:22 -0500

    add the in class version of choose game difficulty

---
+++ in-class-assignments/choose-game-difficulty-2/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title: Choose Game Difficulty
+ * Assignment Description: Using a loop to get user input
+ * Due Date: 09/16/2024
+ * Date Created: 09/16/2024
+ * Date Last Modified: 09/16/2024
+ */
+#include <iostream>
+
+using namespace std;
+int main() {
+  // Data Abstraction:
+    
+  // Input:
+    
+  // Process:
+    
+  // Output:
+    
+  // Assumptions:
+
+  return 0;
+}

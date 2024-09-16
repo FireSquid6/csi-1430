6f47ef39c48154c8ce2a6ca2ecdc58e7307498a0
firesquid6 <jdeiss06@gmail.com>
2024-09-16 08:55:37 -0500

    add highway number

--- scratch/heap-string/main.cpp
+++ scratch/heap-string/main.cpp
@@ -1,24 +1,45 @@
-/*
- * Author: Jonathan Deiss
- * Assignment Title:
- * Assignment Description:
- * Due Date:
- * Date Created:
- * Date Last Modified:
- */
 #include <iostream>
-
 using namespace std;
+
 int main() {
-  // Data Abstraction:
-    
-  // Input:
-    
-  // Process:
-    
-  // Output:
-    
-  // Assumptions:
+  int highwayNum;
+  int primaryHighwayNum;
+  int auxiliaryHighwayNum;
+
+  cin >> highwayNum;
+
+  // Check if highwayNum is in valid range
+  if (highwayNum < 1 || highwayNum > 999) {
+    cout << highwayNum << " is not a valid interstate highway number." << endl;
+  } else {
+    // Check if highwayNum is primary or auxiliary
+    if (highwayNum >= 1 && highwayNum <= 99) {
+      cout << "I-" << highwayNum << " is primary, going ";
+      if (highwayNum % 2 == 0) {
+        cout << "east/west." << endl;
+      } else {
+        cout << "north/south." << endl;
+      }
+    } else {
+      // Extract primary and auxiliary highway numbers
+      primaryHighwayNum = highwayNum % 100;
+      auxiliaryHighwayNum = highwayNum / 100;
+
+      // Check if primaryHighwayNum is valid
+      if (primaryHighwayNum == 0) {
+        cout << highwayNum << " is not a valid interstate highway number."
+             << endl;
+      } else {
+        cout << "I-" << highwayNum << " is auxiliary, serving I-"
+             << primaryHighwayNum << ", going ";
+        if (primaryHighwayNum % 2 == 0) {
+          cout << "east/west." << endl;
+        } else {
+          cout << "north/south." << endl;
+        }
+      }
+    }
+  }
 
   return 0;
 }

4c44abb74778ba586d60feeddc0fa8dd8c4633c1
firesquid6 <jdeiss06@gmail.com>
2024-09-18 09:34:40 -0500

    finish count vowels program

--- in-class-assignments/count-vowels/main.cpp
+++ in-class-assignments/count-vowels/main.cpp
@@ -11,12 +11,28 @@
 using namespace std;
 int main() {
   // Data Abstraction:
+  string input;
+  char vowels[] = {'a', 'e', 'i', 'o', 'u'};
+  int count = 0;
     
   // Input:
+  getline(cin, input);
     
   // Process:
+  for (int i = 0; i < input.length(); i++) {
+    char c = input[i];
+
+    for (int j = 0; j < 5; j++) {
+      if (c == vowels[j] || c == vowels[j] - 32) {
+        count += 1;
+        break;
+      }
+    }
+    
+  }
     
   // Output:
+  cout << count << endl;
     
   // Assumptions:
 

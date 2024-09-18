99bcea14943d0aa364b7684297db080a374424db
firesquid6 <jdeiss06@gmail.com>
2024-09-18 09:41:37 -0500

    add file stuff

--- in-class-assignments/file-stuff/main.cpp
+++ in-class-assignments/file-stuff/main.cpp
@@ -7,17 +7,25 @@
  * Date Last Modified:
  */
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 int main() {
   // Data Abstraction:
+  ifstream inFile;
+  string fileName = "file.txt";
+
+  int value;
     
   // Input:
     
   // Process:
+  inFile.open(fileName);
+  inFile >> value;
     
   // Output:
-    
+  cout << value << endl;
+
   // Assumptions:
 
   return 0;

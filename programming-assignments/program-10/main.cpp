35e61c8234caaccabfe300d095f2f3fed8284e79
firesquid6 <jdeiss06@gmail.com>
2024-09-24 21:04:01 -0500

    add stuff

--- programming-assignments/program-10/main.cpp
+++ programming-assignments/program-10/main.cpp
@@ -1,116 +1,82 @@
-/*
- * Author: Jonathan Deiss
- * Assignment Title:
- * Assignment Description:
- * Due Date:
- * Date Created:
- * Date Last Modified:
- */
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <string>
 
 using namespace std;
+
 int main() {
-  // Data Abstraction:
-  string fileName;
-  string mode;
-  bool flag = true;
-  char c;
-  fstream inFile;
-  fstream outFile;
-  string output = "";
+  string filename, command;
+  cout << "Enter File Name: ";
+  cin >> filename;
+  cout << "Enter encrypt or decrypt: ";
+  cin >> command;
+
+  if (command != "encrypt" && command != "decrypt") {
+    cout << "Error: Bad Command.\n";
+    return 1;
+  }
+
+  ifstream file(filename);
+  if (!file.is_open()) {
+    cout << "Error: File did NOT open.\n";
+    return 1;
+  }
+
+  ofstream outputFile("output");
+  if (!outputFile.is_open()) {
+    cout << "Error: Failed to open output file.\n";
+    return 1;
+  }
 
   int aCount = 0;
+  int eCount = 0;
+  int iCount = 0;
   int oCount = 0;
   int uCount = 0;
-  int iCount = 0;
-  int eCount = 0;
   int yCount = 0;
-    
-  // Input:
-  cout << "Enter File Name: ";
-  cin >> fileName;
-  cout << fileName << endl;
-  cout << "Enter encrypt or decrypt: ";
-  cin >> mode;
-  cout << mode << endl;
-    
-  // Process:
-  inFile.open(fileName);
 
-  if (!inFile.is_open()) {
-    cout << "Error: File did NOT open.";
-    flag = false;
-  }
-
-  if (mode != "encrypt" && mode != "decrypt") {
-    cout << "Error: Bad Command.";
-    flag = false;
-  }
-
-  outFile.open("output");
-
-  if (!outFile.is_open()) {
-    cout << "Error: Output file did NOT open.";
-    flag = false;
-  }
-
-
-  if (flag) {
-    while (inFile.get(c)) {
-      switch (toupper(c)) {
-        case 'A': {
-          aCount += 1;
-          break;
-        }
-        case 'E': {
-          eCount += 1;
-          break;
-        }
-        case 'I': {
-          iCount += 1;
-          break;
-        }
-        case 'O': {
-          oCount += 1;
-          break;
-        }
-        case 'U': {
-          uCount += 1;
-          break;
-        }
-        case 'Y': {
-          yCount += 1;
-          break;
-        }
-      }
+  char c;
+  char newC;
+  while (file.get(c)) {
+    if (command == "encrypt") {
+      newC = c + 3;
+    } else {
+      newC = c - 3;
     }
 
-    if (mode == "encrypt") {
-      cout << (c + 3);
-      output.push_back(c + 3);
+    switch (toupper(c)) {
+    case 'A':
+      aCount++;
+      break;
+    case 'E':
+      eCount++;
+      break;
+    case 'I':
+      iCount++;
+      break;
+    case 'O':
+      oCount++;
+      break;
+    case 'U':
+      uCount++;
+      break;
+    case 'Y':
+      yCount++;
+      break;
     }
-    else {
-      output.push_back(c - 3);
-    }
 
-    // Output
-    cout << output << endl;
-    outFile << output << endl;
+    cout << newC;
+    outputFile << newC;
 
-
-    cout << "Letter Frequency" << endl;
-    cout << "  A    " << aCount << endl;
-    cout << "  E    " << eCount << endl;
-    cout << "  I    " << iCount << endl;
-    cout << "  O    " << oCount << endl;
-    cout << "  U    " << uCount << endl;
-    cout << "  Y    " << yCount << endl;
   }
 
-  inFile.close();
-  outFile.close();
-  // Assumptions:
+  cout << "Frequency of vowels:\n";
+  cout << "   A     " << aCount << "\n";
+  cout << "   E     " << eCount << "\n";
+  cout << "   I     " << iCount << "\n";
+  cout << "   O     " << oCount << "\n";
+  cout << "   U     " << uCount << "\n";
+  cout << "   Y     " << yCount << "\n";
 
   return 0;
 }

7947d7306ffa785e4eadefcb037248fef73eced8
firesquid6 <jdeiss06@gmail.com>
2024-09-25 10:49:55 -0500

    make the code worse for professor's tastes

--- programming-assignments/program-10/main.cpp
+++ programming-assignments/program-10/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title: Program 10
+ * Assignment Description: Caesar Cipher
+ * Due Date: 09/27/2024
+ * Date Created: 09/25/2024
+ * Date Last Modified: 09/25/2024
+ */
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -5,7 +13,7 @@
 using namespace std;
 
 int main() {
-  string filename; 
+  string filename;
   string command;
   int aCount = 0;
   int eCount = 0;
@@ -18,6 +26,7 @@ int main() {
   char c;
   char newC;
 
+  bool flag = true;
 
   cout << "Enter File Name: ";
   cin >> filename;
@@ -26,64 +35,63 @@ int main() {
 
   if (command != "encrypt" && command != "decrypt") {
     cout << "Error: Bad Command.\n";
-    return 1;
+    flag = false;
   }
 
   inputFile.open(filename);
   if (!inputFile.is_open()) {
     cout << "Error: File did NOT open.\n";
-    return 1;
+    flag = false;
   }
 
   outputFile.open("output.txt");
   if (!outputFile.is_open()) {
     cout << "Error: Failed to open output file.\n";
-    return 1;
+    flag = false;
   }
 
+  if (flag) {
 
+    while (inputFile.get(c)) {
+      if (command == "encrypt") {
+        newC = c + 3;
+      } else {
+        newC = c - 3;
+      }
 
+      switch (toupper(c)) {
+      case 'A':
+        aCount++;
+        break;
+      case 'E':
+        eCount++;
+        break;
+      case 'I':
+        iCount++;
+        break;
+      case 'O':
+        oCount++;
+        break;
+      case 'U':
+        uCount++;
+        break;
+      case 'Y':
+        yCount++;
+        break;
+      }
 
-  while (inputFile.get(c)) {
-    if (command == "encrypt") {
-      newC = c + 3;
-    } else {
-      newC = c - 3;
+      cout << newC;
+      outputFile << newC;
     }
-
-    switch (toupper(c)) {
-    case 'A':
-      aCount++;
-      break;
-    case 'E':
-      eCount++;
-      break;
-    case 'I':
-      iCount++;
-      break;
-    case 'O':
-      oCount++;
-      break;
-    case 'U':
-      uCount++;
-      break;
-    case 'Y':
-      yCount++;
-      break;
-    }
-
-    cout << newC;
-    outputFile << newC;
-
+    cout << endl;
+    cout << "Frequency of vowels:" << endl;
+    cout << "   A     " << aCount << endl;
+    cout << "   E     " << eCount << endl;
+    cout << "   I     " << iCount << endl;
+    cout << "   O     " << oCount << endl;
+    cout << "   U     " << uCount << endl;
+    cout << "   Y     " << yCount << endl;
   }
-  cout << endl;
-  cout << "Frequency of vowels:" << endl;
-  cout << "   A     " << aCount << endl;
-  cout << "   E     " << eCount << endl;
-  cout << "   I     " << iCount << endl;
-  cout << "   O     " << oCount << endl;
-  cout << "   U     " << uCount << endl;
-  cout << "   Y     " << yCount << endl;
 
   outputFile.close();
   inputFile.close();

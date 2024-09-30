e78434bc4ed021e306567e94b03aed3f2e6c364a
firesquid6 <jdeiss06@gmail.com>
2024-09-30 09:10:55 -0500

    add stuff

--- scratch/stupid-multiplication/main.cpp
+++ scratch/stupid-multiplication/main.cpp
@@ -10,12 +10,22 @@ int main() {
   int n4 = 999;
   int n5 = 999;
 
+  long n1_long = n1;
+  long n2_long = n2;
+  long n3_long = n3;
+  long n4_long = n4;
+  long n5_long = n5;
+
   long int_biggest = INT_MAX;  // the largest number an int can hold
   long biggest = LONG_MAX;  // the largest number a long can hold
-  long badResult = n1 * n2 * n3 * n4 * n5;  // this is overflowing for some reason
+
+  long badResult = n1 * n2 * n3 * n4 * n5;  // n1 * n2 * n3 * n4 * n5 is too big for an int to hold
   long mostResult = n1 * n2 * n3;  // this is the most I can get without it overflowing
+  long otherGoodResult = n1_long * n2_long * n3_long * n4_long * n5_long;
+
+  // but over here it's fine because each int is typecasted to a long implicity before
+  // being multiplied
   long goodResult = n1;
-  // implicity typecasting?
   goodResult *= n2;
   goodResult *= n3;
   goodResult *= n4;
@@ -25,12 +35,7 @@ int main() {
   cout << "long biggest: " << biggest << endl;
   cout << "badResult: " << badResult << endl;
   cout << "goodResult: " << goodResult << endl;
+  cout << "otherGoodResult: " << otherGoodResult << endl;
   
-  if (goodResult > biggest) {
-    cout << "goodResult is bigger than biggest" << endl;
-  } else {
-    cout << "goodResult is not bigger than biggest" << endl;
-  }
-
   return 0;
 }

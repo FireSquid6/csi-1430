795ce44b14751134f5205c57847cda479595a3fe
firesquid6 <jdeiss06@gmail.com>
2024-09-30 22:31:50 -0500

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "$0")/.." || exit
+
+NAME=$1
+
 if [ ! -f ./main.cpp ]; then
-  echo "no main.cpp. Are you in the right directory?"
-  exit 1
+  echo "Finding the correct directory..."
 fi
+
+if [ -f "./programming-assignments/$NAME/main.cpp" ]; then
+  cd "./programming-assignments/$NAME" || exit
+fi
+
+if [ -f "./in-class-assignments/$NAME/main.cpp" ]; then
+  cd "./in-class-assignments/$NAME" || exit
+fi
+
+if [ -f "./scratch/$NAME/main.cpp" ]; then
+  cd "./scratch/$NAME" || exit
+fi
+
 
 echo "COMPILING..."
 g++ ./main.cpp -o program.o

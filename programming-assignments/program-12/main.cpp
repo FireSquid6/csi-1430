1645e3e96dd3666700fd27e7c265f00e626b61b6
firesquid6 <jdeiss06@gmail.com>
2024-09-30 23:12:46 -0500

    add the basics of program 12

--- programming-assignments/program-12/main.cpp
+++ programming-assignments/program-12/main.cpp
@@ -7,16 +7,62 @@
  * Date Last Modified: 09/30/2024
  */
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 int main() {
   // Data Abstraction:
+  string dataFilename;
+  ifstream dataFile;
+  bool continueLoop = true;
+
+  double minimum = 99999.9;
+  double maximum = -1.0;
+  double averagesTotal = 0;
+  double classAverage;
+  int studentCount = 0;
+
     
   // Input:
+  cout << "Enter Name of Data File: ";
+  cin >> dataFilename;
+  cout << dataFilename << endl;
     
   // Process:
+  dataFile.open(dataFilename);
+
+  if (!dataFile.is_open()) {
+    cout << "Error: File did NOT open." << endl;
+    continueLoop = false;
+    dataFile.close();
+  }
+
+  while (continueLoop) {
+    // Data Abstraction:
+    double scoresTotal;
+    double scoresAverage;
+    int scoresCount;
+    string first;
+    string last;
+    string middle;
+    string scoreFilename;
+    ifstream scoreFile;
     
+    // Process:
+    dataFile >> first;
+    dataFile >> last;
+    dataFile >> middle;
+
+    // Output:
+
+  }
+  classAverage = averagesTotal / studentCount;
+
   // Output:
+  cout << "Class Average " << classAverage << endl;
+  cout << "Max score " << maximum << endl;
+  cout << "Min score " << minimum << endl;
     
   // Assumptions:
 

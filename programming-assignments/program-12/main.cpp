4ff11fd6c494d0c704690e627c71e4bbfba4ede0
firesquid6 <jdeiss06@gmail.com>
2024-10-03 15:10:30 -0500

    add stuff

--- programming-assignments/program-12/main.cpp
+++ programming-assignments/program-12/main.cpp
@@ -6,12 +6,12 @@
  * Date Created: 09/30/2024
  * Date Last Modified: 09/30/2024
  */
-#include <iostream>
-#include <iomanip>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
-
 
 //************************************************************
 // description: Reads the scores from a file
@@ -19,33 +19,31 @@ using namespace std;
 // pre: the scores file exists and is open
 // post: scores file is closed
 //************************************************************
-double getAverage(ifstream scoreFile) {
+double getAverage(ifstream *scoreFile) {
   int scoresTotal = 0;
   int scoresCount = 0;
   int currentScore;
 
-
-  while (scoreFile >> currentScore) {
+  while (*scoreFile >> currentScore) {
     scoresCount += 1;
     scoresTotal += currentScore;
   }
 
+  if (scoresCount == 0) {
+    return -1;
+  }
 
   return static_cast<double>(scoresTotal) / scoresCount;
 }
 
-
 int main() {
   // Data Abstraction:
   string dataFilename;
-  string outputString;
+
   ifstream dataFile;
-  bool continueLoop = true;
   string first;
   string last;
   string middle;
-  string scoreFilename;
-  ifstream scoreFile;
 
   double minimum = 99999.9;
   double maximum = -1.0;
@@ -54,12 +52,10 @@ int main() {
   double currentAverage = 0;
   int studentCount = 0;
 
-    
   // Input:
   cout << "Enter Name of Data File: ";
   cin >> dataFilename;
   cout << dataFilename << endl;
-
 
   // Process:
   dataFile.open(dataFilename);
@@ -71,41 +67,55 @@ int main() {
     return 1;
   }
 
-  while (dataFile >> first) {
-    dataFile >> last;
+  while (dataFile >> last) {
+    // Data Abstraction
+    stringstream outputString;
+    ifstream scoreFile;
+    string scoreFilename;
+
+    // Input
+    dataFile >> first;
     dataFile >> middle;
 
-
     // Process:
-    scoreFilename = last + first + ".dat";
+    scoreFilename = first + last + ".dat";
     scoreFile.open(scoreFilename);
+    outputString << first << " " << middle << " " << last << "    ";
 
     if (scoreFile.is_open()) {
-      studentCount += 1;
-      currentAverage = getAverage(scoreFile);
+
+      currentAverage = getAverage(&scoreFile);
 
       if (currentAverage == -1) {
+        outputString << "No Scores";
+      } else {
+        averagesTotal += currentAverage;
+        studentCount += 1;
 
+        if (currentAverage < minimum) {
+          minimum = currentAverage;
+        }
+        if (currentAverage > maximum) {
+          maximum = currentAverage;
+        }
+
+        outputString << currentAverage;
       }
-
+      scoreFile.close();
     } else {
-      outputString = "No Data File";
+      outputString << "No Data File";
     }
 
     // Output:
-
+    cout << outputString.str() << endl;
   }
   classAverage = averagesTotal / studentCount;
 
   // Output:
-  cout << fixed << setprecision(2) << endl;
+  cout << fixed << setprecision(2);
   cout << "Class Average " << classAverage << endl;
   cout << "Max score " << maximum << endl;
   cout << "Min score " << minimum << endl;
-    
+
   return 0;
 }
-
-
-
-

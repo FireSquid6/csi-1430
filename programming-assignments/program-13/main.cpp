b1f54dac88311c251589207b4eeb7d0b5bd53ac0
firesquid6 <jdeiss06@gmail.com>
2024-10-06 14:40:39 -0500

    add fibonacci array

---
+++ programming-assignments/program-13/main.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title: Fibonacci Array
+ * Assignment Description: Prints the Nth value in the fibonacci sequence
+ * Due Date: 10/10/2024
+ * Date Created: 10/06/2024
+ * Date Last Modified: 10/06/2024
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main() {
+  // Data Abstraction:
+  vector<int> fibinacci = {0, 1};
+  vector<int> requests;
+  int numRequests;
+  int request;
+    
+  // Input:
+  cin >> numRequests;
+
+  for (int i = 0; i < numRequests; i++) {
+    cin >> request;
+    requests.push_back(request);
+  }
+
+  // Process:
+  for (int i = 0; i < 89; i++) {
+    fibinacci.push_back(fibinacci[i] + fibinacci[i + 1]);
+  }
+    
+  // Output:
+  for (int i = 0; i < numRequests; i++) {
+    cout << "Case " << i + 1 << ": Fib(" << requests[i] << ") = " << fibinacci[requests[i]] << endl;
+  }
+    
+  // Assumptions:
+
+  return 0;
+}

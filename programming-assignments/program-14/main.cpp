a111a89ee38d8c9e257695222ef188bb3e9a0049
firesquid6 <jdeiss06@gmail.com>
2024-10-07 09:46:13 -0500

    add program 14

--- programming-assignments/program-14/main.cpp
+++ programming-assignments/program-14/main.cpp
@@ -1,22 +1,49 @@
 /*
- * Author: Jonathan Deiss
- * Assignment Title:
- * Assignment Description:
- * Due Date:
- * Date Created:
- * Date Last Modified:
+ * Author: Jonathan Deiss 
+ * Assignment Title: Array Backwards
+ * Assignment Description: Performs some operations on an array
+ * Due Date: 10/11/2024
+ * Date Created: 10/07/2024
+ * Date Last Modified: 10/07/2024
  */
 #include <iostream>
+#include <vector>
 
 using namespace std;
 int main() {
   // Data Abstraction:
-    
+  vector<int> numbers; 
+  int numInputs;
+    int input;
+
   // Input:
+  cout << "Input the number of elements to store in the array: ";
+  cin >> numInputs;
+  cout << numInputs << endl << endl;
+
+  cout << "Input " << numInputs << " integers: ";
+  for (int i = 0; i < numInputs; i++) {
+    cout << "integer - " << i << " : ";
+    cin >> input;
+    cout << input << endl;
+    numbers.push_back(input);
+  }
     
   // Process:
     
   // Output:
+  cout << endl;
+  cout << "The values stored into the array are : " << endl;
+  for (int i = 0; i < numInputs; i++) {
+    cout << numbers[i] << " ";
+  }
+  cout << endl << endl;
+
+  cout << "The values stored into the array in reverse are : " << endl;
+  for (int i = numInputs - 1; i >= 0; i--) {
+    cout << numbers[i] << " ";
+  }
+  cout << endl << endl;
     
   // Assumptions:
 

b5ca44d01a8649d59e9b7ff82f6ef8143e3ef418
firesquid6 <jdeiss06@gmail.com>
2024-10-15 14:03:56 -0500

    fix the stuff

--- programming-assignments/program-15/main.cpp
+++ programming-assignments/program-15/main.cpp
@@ -12,43 +12,57 @@
 using namespace std;
 int main() {
   // Data Abstraction:
-  int size;
-  int maximum;
+  vector<int> sizes;
+  int sizeInput;
+  bool flag = true;
   
   // Input:
-  cin >> size;
-
-  // Matrix Definition (has to happen after input)
-  int matrix[size][size];
-
-  // Process
-  maximum = size / 2;
-    
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      int x = i;
-      int y = j;
-
-      if (x > maximum) {
-        x = size - x - 1;
-      }
-      if (y > maximum) {
-        y = size - y - 1;
-      }
-
-      if (x < y) {
-        matrix[i][j] = x + 1;
-      } else {
-        matrix[i][j] = y + 1;
-      }
+  while (flag == true) {
+    cin >> sizeInput;
+    if (sizeInput != 0) {
+      sizes.push_back(sizeInput);
+    } else {
+      flag = false;
     }
-
   }
-    
-  // Output:
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      cout << matrix[i][j] << " ";
+
+  // Loop
+  for (int k = 0; k < sizes.size(); k++) {
+    // Data Abstraction:
+    int size = sizes[k];
+    int matrix[size][size];
+    int maximum;
+
+    // Process
+    maximum = size / 2 - 1;
+      
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        int x = i;
+        int y = j;
+
+        if (x > maximum) {
+          x = size - x - 1;
+        }
+        if (y > maximum) {
+          y = size - y - 1;
+        }
+
+        if (x < y) {
+          matrix[i][j] = x + 1;
+        } else {
+          matrix[i][j] = y + 1;
+        }
+      }
+
+    }
+      
+    // Output:
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        cout << matrix[i][j] << " ";
+      }
+      cout << endl;
     }
     cout << endl;
   }

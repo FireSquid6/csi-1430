9c00ae1af93273895388def58179ce370107c670
firesquid6 <jdeiss06@gmail.com>
2024-10-21 20:05:50 -0500

    add block comments for cstirng.h

--- programming-assignments/program-18/cstring.h
+++ programming-assignments/program-18/cstring.h
@@ -10,34 +10,34 @@
 #define CSTRING_H
 
 //************************************************************
-// description: 
-// return: 
-// pre: 
-// post: 
+// description: calculates the length of a string
+// return: int
+// pre: a string s1 exists
+// post: the length of s1 is returned
 //************************************************************
 int Strlen(const char s1[]);
 
 //************************************************************
-// description: 
-// return: 
-// pre: 
-// post: 
+// description: copies the contents of s2 into s1
+// return: void
+// pre: a string s1 and s2 exist
+// post: s1 is now equal to s2 
 //************************************************************
 void Strcpy(char s1[], const char s2[]);
 
 //************************************************************
-// description: 
-// return: 
-// pre: 
-// post: 
+// description: compares two strings 
+// return: int
+// pre: strings s1 and s2 exist
+// post: 0 or the difference between the first different character is returned
 //************************************************************
 int Strcmp(const char s1[], const char s2[]);
 
 //************************************************************
-// description: 
-// return: 
-// pre: 
-// post: 
+// description: concatenates s2 onto s1
+// return: void
+// pre: strings s1 and s2 exist
+// post: s2 is now appended to s1
 //************************************************************
 void Strcat(char s1[], const char s2[]);
 

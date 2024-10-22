695731135d90364cbbaf161fdf28841bcd339134
firesquid6 <jdeiss06@gmail.com>
2024-10-21 20:39:03 -0500

    refactor to avoid using a return

--- programming-assignments/program-18/cstring.cpp
+++ programming-assignments/program-18/cstring.cpp
@@ -28,14 +28,21 @@ void Strcpy(char s1[], const char s2[]) {
 
 int Strcmp(const char s1[], const char s2[]) {
   int i = 0;
+  int difference = 0;
+  bool foundDifference = false;
   while (s1[i] != '\0' && s2[i] != '\0') {
-    if (s1[i] != s2[i]) {
-      return s1[i] - s2[i];
+    if (s1[i] != s2[i] && !foundDifference) {
+      foundDifference = true;
+      difference = s1[i] - s2[i];
     }
     i++;
   }
 
-  return s1[i] - s2[i];
+  if (!foundDifference) {
+    difference = s1[i] - s2[i];
+  }
+
+  return difference;
 }
 
 void Strcat(char s1[], const char s2[]) {

ac7f416738f8e8112935639befe93b0023db47c6
firesquid6 <jdeiss06@gmail.com>
2024-10-21 19:23:07 -0500

    add an assignment header

--- programming-assignments/program-17/myFunctions.cpp
+++ programming-assignments/program-17/myFunctions.cpp
@@ -1,4 +1,11 @@
-// TODO - finish documentation
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title: My Functions
+ * Assignment Description: Math functions
+ * Due Date: 10/23/2024
+ * Date Created: 10/21/2024
+ * Date Last Modified: 10/21/2024
+ */
 #include "myFunctions.h"
 
 // Note - these functions could be implemented using a
@@ -34,7 +41,7 @@ double max(double a, double b) {
 
 // same thing about type generics applies here
 int max(int list[], int size) {
-  // setting to the first value is 
+  // setting to the first value is
   // dangerous since list could be empty
   bool setMax = false;
   int maximum;
@@ -46,7 +53,6 @@ int max(int list[], int size) {
     } else if (list[i] > maximum) {
       maximum = list[i];
     }
-
   }
 
   // This means the list was empty
@@ -56,7 +62,6 @@ int max(int list[], int size) {
 
   return maximum;
 }
-
 
 double max(double list[], int size) {
   bool setMax = false;
@@ -69,7 +74,6 @@ double max(double list[], int size) {
     } else if (list[i] > maximum) {
       maximum = list[i];
     }
-
   }
 
   if (!setMax) {
@@ -77,4 +81,86 @@ double max(double list[], int size) {
   }
 
   return maximum;
+}
+
+int min(int a, int b) {
+  int minimum = a;
+  if (b < a) {
+    minimum = b;
+  }
+
+  return minimum;
+}
+
+double min(double a, double b) {
+  double minimum = a;
+  if (b < a) {
+    minimum = b;
+  }
+
+  return minimum;
+}
+
+int min(int list[], int size) {
+  bool setMin = false;
+  int minimum;
+
+  for (int i = 0; i < size; i++) {
+    if (!setMin) {
+      minimum = list[i];
+      setMin = true;
+    } else if (list[i] < minimum) {
+      minimum = list[i];
+    }
+  }
+
+  return minimum;
+}
+
+double min(double list[], int size) {
+  bool setMin = false;
+  double minimum;
+
+  for (int i = 0; i < size; i++) {
+    if (!setMin) {
+      minimum = list[i];
+      setMin = true;
+    } else if (list[i] < minimum) {
+      minimum = list[i];
+    }
+  }
+
+  return minimum;
+}
+
+int absoluteValue(int a) {
+  if (a < 0) {
+    return -a;
+  }
+
+  return a;
+}
+
+double absoluteValue(double a) {
+  if (a < 0) {
+    return -a;
+  }
+
+  return a;
+}
+
+int factorial(int n) {
+  if (n == 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+double combination(int n, int r) {
+  return factorial(n) / static_cast<double>((factorial(r) * factorial(n - r)));
+}
+
+double permutation(int n, int r) { 
+  return factorial(n) / static_cast<double>(factorial(n - r)); 
 }
--- programming-assignments/program-17/myFunctions.h
+++ programming-assignments/program-17/myFunctions.h
@@ -1,3 +1,11 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title: My Functions
+ * Assignment Description: Math functions
+ * Due Date: 10/23/2024
+ * Date Created: 10/21/2024
+ * Date Last Modified: 10/21/2024
+ */
 #ifndef MYFUNCTIONS_H
 #define MYFUNCTIONS_H
 
@@ -5,5 +13,20 @@ int max(int, int);
 double max(double, double);
 int max(int[], int);
 double max(double[], int);
+
+int min(int, int);
+double min(double, double);
+int min(int[], int);
+double min(double[], int);
+
+int absoluteValue(int);
+double absoluteValue(double);
+
+
+int factorial(int);
+
+double combination(int, int);
+
+double permutation(int, int);
 
 #endif

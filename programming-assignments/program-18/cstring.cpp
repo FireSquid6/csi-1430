d8d55d182400d0d0ea7ab1ca184b336a8ccaa1c3
firesquid6 <jdeiss06@gmail.com>
2024-10-21 19:28:36 -0500

    add headers to both files for program 18

---
+++ programming-assignments/program-18/cstring.cpp
@@ -0,0 +1,8 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title: C-style String
+ * Assignment Description: C-style string functions
+ * Due Date: 10/25/2024
+ * Date Created: 10/21/2024
+ * Date Last Modified: 10/21/2024
+ */
---
+++ programming-assignments/program-18/cstring.h
@@ -0,0 +1,17 @@
+/*
+ * Author: Jonathan Deiss
+ * Assignment Title: C-style String
+ * Assignment Description: C-style string functions
+ * Due Date: 10/25/2024
+ * Date Created: 10/21/2024
+ * Date Last Modified: 10/21/2024
+ */
+#ifndef CSTRING_H
+#define CSTRING_H
+
+int Strlen(const char s1[]);
+void Strcpy(char s1[], const char s2[]);
+int Strcmp(const char s1[], const char s2[]);
+void Strcat(char s1[], const char s2[]);
+
+#endif

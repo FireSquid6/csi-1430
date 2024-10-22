43eaf2e21a922ed17666f18a7ef15d00e7b46e77
firesquid6 <jdeiss06@gmail.com>
2024-10-21 19:34:58 -0500

    add cstring implementation

--- programming-assignments/program-18/cstring.cpp
+++ programming-assignments/program-18/cstring.cpp
@@ -6,7 +6,7 @@
  * Date Created: 10/21/2024
  * Date Last Modified: 10/21/2024
  */
-
+// TODO - finish documentation
 
 int Strlen(const char s1[]) {
   int i = 0;
@@ -17,4 +17,38 @@ int Strlen(const char s1[]) {
   }
   
   return i;
+}
+
+void Strcpy(char s1[], const char s2[]) {
+  int i = 0;
+  while (s2[i] != '\0') {
+    s1[i] = s2[i];
+    i++;
+  }
+}
+
+// TODO - refactor this to not use an early return
+int Strcmp(const char s1[], const char s2[]) {
+  int i = 0;
+  while (s1[i] != '\0' && s2[i] != '\0') {
+    if (s1[i] != s2[i]) {
+      return s1[i] - s2[i];
+    }
+    i++;
+  }
+  return s1[i] - s2[i];
+}
+
+void Strcat(char s1[], const char s2[]) {
+  int i = 0;
+  while (s1[i] != '\0') {
+    i++;
+  }
+  int j = 0;
+  while (s2[j] != '\0') {
+    s1[i] = s2[j];
+    i++;
+    j++;
+  }
+  s1[i] = '\0';
 }

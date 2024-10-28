c08a8e26e6805dcf041fc25d7d07ae51fbbe2720
firesquid6 <jdeiss06@gmail.com>
2024-10-28 09:23:39 -0500

    add repetitive block comments

--- programming-assignments/program-19/arrayCount.h
+++ programming-assignments/program-19/arrayCount.h
@@ -19,82 +19,82 @@ using namespace std;
 const int MAX_SIZE = 25;
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
+// description: Counts the number of `v` in `arr` of length `s`
+// return: int
+// pre: arr exists and is of size s
+// post: return value is the number of v in arr
 //************************************************************
 int arrayCount(int arr[], int s, int v);
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
+// description: Counts the number of `v` in `arr` of length `s`
+// return: int
+// pre: arr exists and is of size s
+// post: return value is the number of v in arr
 //************************************************************
 int arrayCount(double a[], int s, double v);
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
-//************************************************************
+// description: Counts the number of `v` in `arr` of length `s`
+// return: int
+// pre: arr exists and is of size s
+// post: return value is the number of v in arr
+// ************************************************************
 int arrayCount(char a[], char v);
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
-//************************************************************
+// description: Counts the number of `v` in `a` vector 
+// return: int
+// pre: vector exists 
+// post: return value is the number of v in a
+// ************************************************************
 int arrayCount(vector<int> a, int v);
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
-//************************************************************
+// description: Counts the number of `v` in `a` vector 
+// return: int
+// pre: vector exists 
+// post: return value is the number of v in a
+// ************************************************************
 int arrayCount(vector<string> a, string v);
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
-//************************************************************
+// description: Counts the number of `v` in `a` vector 
+// return: int
+// pre: vector exists 
+// post: return value is the number of v in a
+// ************************************************************
 int arrayCount(vector<double> a, double v);
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
-//************************************************************
+// description: Counts the number of `v` in `a` vector 
+// return: int
+// pre: vector exists 
+// post: return value is the number of v in a
+// ************************************************************
 int arrayCount(vector<char> a, char v);
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
+// description: Counts the number of `v` in `m` of size `r` by `c`
+// return: int
+// pre: matrix exists and is of row r and column c
+// post: return value is the number of v in m
 //************************************************************
 int matrixCount(int m[][MAX_SIZE], int r, int c, int v);
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
+// description: Counts the number of `v` in `m` of size `r` by `c`
+// return: int
+// pre: matrix exists and is of row r and column c
+// post: return value is the number of v in m
 //************************************************************
 int matrixCount(char m[][MAX_SIZE], int r, int c, int v);
 
 //************************************************************
-// description:
-// return: 
-// pre: 
-// post:
+// description: Counts the number of `v` in `m` of size `r` by `c`
+// return: int
+// pre: matrix exists and is of row r and column c
+// post: return value is the number of v in m
 //************************************************************
 int matrixCount(string m[][MAX_SIZE], int r, int c, string v);
 

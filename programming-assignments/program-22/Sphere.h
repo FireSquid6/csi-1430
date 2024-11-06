027194854a5091fe54a8a16205234b5c5099c1b8
firesquid6 <jdeiss06@gmail.com>
2024-11-06 09:06:35 -0600

    add stupid comment blocks

--- programming-assignments/program-22/Sphere.h
+++ programming-assignments/program-22/Sphere.h
@@ -18,19 +18,79 @@ private:
   double radius;
   string color;
 public:
+  //************************************************************
+  // description: Creates a new sphere
+  // return: Sphere
+  // pre: none
+  // post: a sphere with radius 0 and color "green" is created
+  //************************************************************
   Sphere();
+  //************************************************************
+  // description: Creates a new sphere with radius r
+  // return: Sphere
+  // pre: a double r exists
+  // post: a sphere with radius r is created
+  //************************************************************
   Sphere(double r);
+  //************************************************************
+  // description: Creates a new sphere
+  // return: Sphere
+  // pre: a double r and string c exist
+  // post: a sphere with radius r and color c is created
+  //************************************************************
   Sphere(double r, string c);
 
+  //************************************************************
+  // description: Sets the radius of a sphere
+  // return: void
+  // pre: a sphere exists
+  // post: the sphere has the radius set to the double
+  //************************************************************
   void setRadius(double);
+  //************************************************************
+  // description: gets the radius of a sphere
+  // return: double
+  // pre: a sphere exists
+  // post: none
+  //************************************************************
   double getRadius() const;
 
+  //************************************************************
+  // description: Sets the color of a sphere
+  // return: void
+  // pre: a sphere exists
+  // post: the sphere has the color set to the string
+  //************************************************************
   void setColor(string);
+  //************************************************************
+  // description: Gets the color of a sphere
+  // return: string
+  // pre: a sphere exists
+  // post: none
+  //************************************************************
   string getColor() const;
 
+  //************************************************************
+  // description: Calculates the area of a sphere 
+  // return: double
+  // pre: a sphere exists
+  // post: none
+  //************************************************************
   double area() const;
+  //************************************************************
+  // description: Calculates the volume of a sphere
+  // return: double
+  // pre: a sphere exists
+  // post: none
+  //************************************************************
   double volume() const;
 
+  //************************************************************
+  // description: Checks if two spheres are equal 
+  // return: bool
+  // pre: two spheres exist
+  // post: both spheres are unmodified
+  //************************************************************
   bool isEqual(const Sphere) const;
 };
 

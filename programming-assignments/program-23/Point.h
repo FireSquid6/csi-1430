fe5201b63a61a6da894862f01b40f6b9c271a9a2
firesquid6 <jdeiss06@gmail.com>
2024-11-11 09:21:46 -0600

    fix some formatting

--- programming-assignments/program-23/Point.h
+++ programming-assignments/program-23/Point.h
@@ -5,27 +5,25 @@
 
 using namespace std;
 
-struct Point{
-   double x,y;
+struct Point {
+  double x, y;
 
-   Point(const Point& p){
-       *this = p;
-   }
+  Point(const Point &p) { *this = p; }
 
-   Point(double x = 0, double y = 0){
-      this->x = x;
-      this->y = y;
-   }
+  Point(double x = 0, double y = 0) {
+    this->x = x;
+    this->y = y;
+  }
 
-   Point& operator= (const Point& p){
-       x = p.x;
-       y = p.y;
-       return *this;
-   }
+  Point &operator=(const Point &p) {
+    x = p.x;
+    y = p.y;
+    return *this;
+  }
 
-   double distance(Point p) const{
-       return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
-   }
+  double distance(Point p) const {
+    return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
+  }
 };
 
 #endif

b1f95bd08eba0ca195d96e7f2f3af98f9d33bcec
firesquid6 <jdeiss06@gmail.com>
2024-11-11 09:21:34 -0600

    format line

--- programming-assignments/program-23/Line.h
+++ programming-assignments/program-23/Line.h
@@ -1,37 +1,36 @@
 #ifndef LINE_H_
 #define LINE_H_
 
-#include <ostream>
-#include <iostream>
 #include "Point.h"
+#include <iostream>
+#include <ostream>
 
 using namespace std;
 
+class Line {
+private:
+  Point p1, p2;
 
-class Line{
-	private:
-		Point p1, p2;
+public:
+  Line(Point = Point(1, 1), Point = Point(0, 0));
 
-	public:
-		Line(Point=Point(1,1), Point=Point(0,0));
+  void setFirstPoint(const Point &);
+  void setSecondPoint(const Point &);
 
- 		void setFirstPoint(const Point&);
-		void setSecondPoint(const Point&);
+  Point getFirstPoint() const;
+  Point getSecondPoint() const;
 
-		Point getFirstPoint() const;
-		Point getSecondPoint() const;
+  bool hasSlope() const;
+  double slope() const;
+  double yIntercept() const;
 
-		bool   hasSlope() const;
-                double slope() const;
-		double yIntercept() const;
+  bool isParallel(const Line &) const;
+  bool isCollinear(const Line &) const;
+  bool isPerpendicular(const Line &) const;
 
-		bool isParallel(const Line&) const;
-		bool isCollinear(const Line&) const;
-		bool isPerpendicular(const Line&) const;
+  Point intersect(const Line &) const;
 
-		Point intersect(const Line&) const;
-
-		void display(ostream&) const;
+  void display(ostream &) const;
 };
 
 #endif /* LINE_H_ */

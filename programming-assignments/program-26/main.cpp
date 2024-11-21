effcfd6f8727042bee118bb71d8cb7ff7611b0e1
firesquid6 <jdeiss06@gmail.com>
2024-11-21 10:45:32 -0600

    make the input work proper

--- programming-assignments/program-26/main.cpp
+++ programming-assignments/program-26/main.cpp
@@ -10,22 +10,102 @@
 #include <string>
 #include <sstream>
 #include "Line.h"
+#include "Point.h"
 
 using namespace std;
 
 
+bool isNumeric(string s) {
+  bool result = true;
+  string validChars = "1234567890-";
+  
+  for (unsigned int i = 0; i < s.length(); i++) {
+    char c = s.at(i);
+
+    if (validChars.find(c) == string::npos) {
+      result = false;
+    }
+  }
+
+  return result;
+}
+
+
+Line parseEquation(string text) {
+  // assumes that the string is valid. This means the following forms (n = any number, * = '+' or '-'):
+  // y = nx + n
+  // y = nx
+  // x = n
+  Line l;
+  double slope = 0;
+  double intercept = 0;
+  
+  bool seenIntercept = false;
+  bool seenY = false;
+  bool nextNegative = false;
+
+  stringstream ss;
+  string token;  
+  ss << text;
+
+  while (ss >> token) {
+    cout << "PARSING: " << token << endl;
+    if (token.at(token.length() - 1) == 'x') {
+      token.pop_back();
+      cout << "  found slope: " << token << endl;
+
+      if (token.length() > 0) {
+        slope = stod(token);
+        if (nextNegative) {
+          slope = -slope;
+        }
+      }
+
+      nextNegative = false;
+    }
+    else if (token.at(token.length() - 1) == 'y') {
+      seenY = true;
+      nextNegative = false;
+    }
+    else if (token == "-") {
+      nextNegative = true;
+    }
+    else if (isNumeric(token)) {
+      cout << "  found intercept: " << token << endl;
+      intercept = stod(token);
+      if (nextNegative) {
+        slope = -slope;
+      }
+      seenIntercept = false;
+    }
+  }
+
+  // y = nx + n or y = nx
+  if (seenY) {
+    l.setFirstPoint(Point(0, intercept));
+    l.setSecondPoint(Point(1, intercept + slope));
+  }
+  else {
+    l.setFirstPoint(Point(intercept, 0));
+    l.setSecondPoint(Point(intercept, 1));
+  }
+
+  return l;
+}
+
 
 int main() {
   // Data Abstraction:
-  string line;
-  stringstream ss;
+  string cinLine;
   Line l1;
   Line l2;
     
   // Input:
-  for (int i = 0; i < 2; i++) {
-    getline(cin, line);
-  }
+  getline(cin, cinLine);
+  l1 = parseEquation(cinLine);
+
+  getline(cin, cinLine);
+  l2 = parseEquation(cinLine);
     
   // Process:
     
